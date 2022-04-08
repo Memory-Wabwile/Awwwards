20f933879c87cd49ce7e6318c5ced6f254778f5d
Memory-Wabwile <memorywabwile@gmail.com>
2022-04-08 11:56:04 +0300

    craete a templates folder and add home.htm file

--- awwwards/views.py
+++ awwwards/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home (request):
+    message = "Landing page"
+    return render (request , 'home.html' , {'message':message})
